126038f7b37ace209d26b37893bb356682354a13
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-11-03 15:05:56 -0500

    Add dependencies and connect backend server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+console.log(PORT);
