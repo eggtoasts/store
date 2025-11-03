8245c0eb04d176e03b45523a6e2bc5f40d092bbb
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-11-03 17:39:33 -0500

    Add bot detection and rate limiting added a bot detection to all routes with arcjet

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -17,6 +18,40 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet()); // security middleware to protect the app by setting HTTP headers
 app.use(morgan("dev")); //log requests to console
+
+//apply bot detection to all routes
+app.use(async (req, res, next) => {
+  try {
+    //each request consumes 1 token
+    const decision = await aj.protect(req, { requested: 1 });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({
+          error: "Too many requests",
+        });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot spotted, denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+
+    //check for a spoofed bot (Acting like a human)
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      res.status(403).json({ error: "Spoofed bot spotted, denied" });
+      return;
+    }
+
+    //can go to next middleware
+    next();
+  } catch (err) {}
+});
 
 app.use("/api/products", productRoutes);
 
